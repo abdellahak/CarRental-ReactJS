58ca6247fad1945d2cb93f44015e2541d85c551b
abdellah khouden <khoudenak@gmail.com>
2025-02-14 00:54:13 +0100

    add react-router-dom and implement routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}>
+          <Route index element={<h1>Home</h1>} />
+          <Route path="/cars" element={<h1>Cars</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+        </Route>
+        <Route path="/dashboard" element={<h1>Dashboard</h1>}>
+          <Route index element={<h1>Statistics</h1>} />
+          <Route path="cars" element={<h1>Cars</h1>} />
+          <Route path="users" element={<h1>Users</h1>} />
+          <Route path="contracts" element={<h1>Contracts</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
