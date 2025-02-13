cbcb8ad98aa112fe26b4fecb9bd7685d50db0c0a
abdellah khouden <khoudenak@gmail.com>
2025-02-13 23:58:37 +0100

    add users reducer

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,17 @@
+
+const init_state = [];
+
+export default function usersReducer(state = init_state, action){
+  switch(action.type){
+    case 'FETCH_USERS_DATA':
+      return action.payload;
+    case 'ADD_USER':
+      return [...state, action.payload];
+    case 'DELETE_USER':
+      return state.filter(user => user.id !== action.payload);
+    case 'UPDATE_USER':
+      return state.map(user => user.id === action.payload.id ? action.payload : user);
+    default : 
+      return state;
+  }
+}
