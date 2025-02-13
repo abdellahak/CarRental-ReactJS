fb4a394c1f120d0066379876ddd14a8769121f4c
abdellah khouden <khoudenak@gmail.com>
2025-02-14 00:10:47 +0100

    set up Redux store with combined reducers and wrap App Component in Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import carsReducer from './reducers/carsReducer.js';
+import usersReducer from './reducers/usersReducer.js';
+import contractsReducer from './reducers/contractsReducer.js';
+import {createStore, combineReducers} from 'redux';
+
+const store = createStore(combineReducers({cars : carsReducer, users : usersReducer, contracts : contractsReducer }));
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
