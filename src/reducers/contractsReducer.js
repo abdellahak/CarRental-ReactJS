61216461889c56d7e07c56f7d21a9d75883ee556
abdellah khouden <khoudenak@gmail.com>
2025-02-18 14:33:59 +0100

    initialize contractsReducer with sample contract data

--- src/reducers/contractsReducer.js
+++ src/reducers/contractsReducer.js
@@ -1,16 +1,59 @@
-const init_state = [];
+const init_state = [
+  {
+    id: "1",
+    userId: "1",
+    carId: "1",
+    startDate: "2025-02-21",
+    endDate: "2025-03-31",
+    price: 300,
+  },
+  {
+    id: "2",
+    userId: "2",
+    carId: "2",
+    startDate: "2025-01-11",
+    endDate: "2025-01-30",
+    price: 160,
+  },
+  {
+    id: "3",
+    userId: "3",
+    carId: "3",
+    startDate: "2025-01-31",
+    endDate: "2025-02-21",
+    price: 140,
+  },
+  {
+    id: "4",
+    userId: "4",
+    carId: "4",
+    startDate: "2025-01-01",
+    endDate: "2025-02-30",
+    price: 170,
+  },
+  {
+    id: "5",
+    userId: "6",
+    carId: "5",
+    startDate: "2025-02-01",
+    endDate: "2025-03-15",
+    price: 180,
+  },
+];
 
-export default function contractsReducer(state = init_state, action){
-  switch(action.type){
-    case 'FETCH_CONTRACTS_DATA':
+export default function contractsReducer(state = init_state, action) {
+  switch (action.type) {
+    case "FETCH_CONTRACTS_DATA":
       return action.payload;
-    case 'ADD_CONTRACT':
+    case "ADD_CONTRACT":
       return [...state, action.payload];
-    case 'DELETE_CONTRACT':
-      return state.filter(contract => contract.id !== action.payload);
-    case 'UPDATE_CONTRACT':
-      return state.map(contract => contract.id === action.payload.id ? action.payload : contract);
-    default : 
+    case "DELETE_CONTRACT":
+      return state.filter((contract) => contract.id !== action.payload);
+    case "UPDATE_CONTRACT":
+      return state.map((contract) =>
+        contract.id === action.payload.id ? action.payload : contract
+      );
+    default:
       return state;
   }
 }
