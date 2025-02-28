f8b33326e27d28334b7b92bfca27034ae996c18e
abdellah khouden <khoudenak@gmail.com>
2025-02-28 01:58:01 +0100

    update Vite configuration and enhance styling with Flowbite integration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+// const flowbite = require("flowbite-react/tailwind");
 // https://vite.dev/config/
 export default defineConfig({
+  // content : [flowbite.content()],
   plugins: [react(), tailwindcss()],
+  // plugins: [react(), tailwindcss(), flowbite.plugin()],
+  // plugins: {'@tailwindcss/postcss': {}},
+  // plugins: [react()],
 })
