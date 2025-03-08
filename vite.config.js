00dd094d9f47dfe9d203d3888631b99c24152129
abdellah khouden <khoudenak@gmail.com>
2025-03-08 14:49:02 +0000

    fix: downgrade date-fns to version 3.6.0 in package.json and package-lock.json; clean up vite.config.js by removing unused plugins

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// const flowbite = require("flowbite-react/tailwind");
-// https://vite.dev/config/
 export default defineConfig({
-  // content : [flowbite.content()],
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -25,7 +22,4 @@ export default defineConfig({
       },
     },
   },
-  // plugins: [react(), tailwindcss(), flowbite.plugin()],
-  // plugins: {'@tailwindcss/postcss': {}},
-  // plugins: [react()],
 });
